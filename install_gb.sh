3285ebeb7b60b22b48a1a67f45d37626829844b1
joongonrigid@gmail.com <joongonrigid@gmail.com>
2023-03-14 00:02:26 +0900

    Telegram bug fix3

--- install_gb.sh
+++ install_gb.sh
@@ -56,7 +56,7 @@ if [ "$loginstat" = "y" ]; then
         read -p "Enter your Telegram TOKEN : " token
         read -p "Enter your Server's Name : " sname
         echo -e "if [ '\$?' = 0 ]; then\ncurl -k -d 'chat_id="$chat_id"' --data-urlencode 'text=GeekBench in "$sname" has been loaded successfully now.' https://api.telegram.org/bot"$token"/sendMessage" >> /etc/cron.daily/geekbench
-        echo -e "elif [ '\$?' = 1 ]; then\ncurl -k -d 'chat_id="$chat_id"' --data-urlencode 'text=It's failed to load GeekBench in "$sname" now. Chek out your configuration.' https://api.telegram.org/bot"$token"/sendMessage\nfi" >> /etc/cron.daily/geekbench   
+        echo -e "elif [ '\$?' = 1 ]; then\ncurl -k -d 'chat_id="$chat_id"' --data-urlencode 'text=It has failed to load GeekBench in "$sname" now. Chek out your configuration.' https://api.telegram.org/bot"$token"/sendMessage\nfi" >> /etc/cron.daily/geekbench   
     else
         echo "no message function"
     fi
