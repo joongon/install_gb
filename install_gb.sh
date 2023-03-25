7175904f01fc744b07bbcd127371e28eeb5a4861
joongonrigid@gmail.com <joongonrigid@gmail.com>
2023-03-25 10:06:59 +0900

    script bug fix

--- install_gb.sh
+++ install_gb.sh
@@ -55,8 +55,8 @@ if [ "$loginstat" = "y" ]; then
         read -p "Enter your Telegram Chat_id : " chat_id
         read -p "Enter your Telegram TOKEN : " token
         read -p "Enter your Server's Name : " sname
-        echo -e "result=$?; if [ $result = 0 ]; then\ncurl -k -d 'chat_id=$chat_id' --data-urlencode 'text=GeekBench in $sname has been loaded successfully now.' https://api.telegram.org/bot$token/sendMessage" >> /etc/cron.daily/geekbench
-        echo -e "elif [ $result = 1 ]; then\ncurl -k -d 'chat_id=$chat_id' --data-urlencode 'text=It has failed to load GeekBench in $sname now. Chek out your configuration.' https://api.telegram.org/bot$token/sendMessage\nfi" >> /etc/cron.daily/geekbench   
+        echo -e "result=\$?\n; if [ \$result = 0 ]; then\ncurl -k -d 'chat_id=$chat_id' --data-urlencode 'text=GeekBench in $sname has been loaded successfully now.' https://api.telegram.org/bot$token/sendMessage" >> /etc/cron.daily/geekbench
+        echo -e "elif [ \$result = 1 ]; then\ncurl -k -d 'chat_id=$chat_id' --data-urlencode 'text=It has failed to load GeekBench in $sname now. Chek out your configuration.' https://api.telegram.org/bot$token/sendMessage\nfi" >> /etc/cron.daily/geekbench   
     else
         echo "no message function"
     fi
